8ab0e375bc0cd47a85cb3689a3ac9409859924b5
Davis Klevgard <davisjklevgard@gmail.com>
2020-06-05 19:15:19 -0500

    Adding what I have done for today

--- src/IntroductionToDataStructuresAndAlgorithmsInJava/AnalysisOfAlgorithms/TimeComplexityOfBubbleSortAlgorithm.java
+++ src/IntroductionToDataStructuresAndAlgorithmsInJava/AnalysisOfAlgorithms/TimeComplexityOfBubbleSortAlgorithm.java
@@ -1,4 +1,12 @@
 package IntroductionToDataStructuresAndAlgorithmsInJava.AnalysisOfAlgorithms;
 
 public class TimeComplexityOfBubbleSortAlgorithm {
+    /*
+    This video is used to describe how a bubble sort algorithm sorts through an array of data.
+    Here is how a bubble sort algorithm works:
+        using a list of 5 numbers, the algorithm looks at the first 2
+        if the first one is higher than the second, the two numbers will be switched
+        This repeats down the line till the highest number is at the end of the list
+        Then the algorithm starts at the beginning of the list and repeats the process till the list is sorted.
+     */
 }
