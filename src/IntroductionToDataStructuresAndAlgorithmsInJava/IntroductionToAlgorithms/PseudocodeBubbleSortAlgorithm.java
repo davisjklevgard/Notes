f645a65a22118917146726e71520d1e8549d86d9
Davis Klevgard <davisjklevgard@gmail.com>
2020-06-10 22:44:32 -0500

    Adding what I have done for today

---
+++ src/IntroductionToDataStructuresAndAlgorithmsInJava/IntroductionToAlgorithms/PseudocodeBubbleSortAlgorithm.java
@@ -0,0 +1,21 @@
+package IntroductionToDataStructuresAndAlgorithmsInJava.IntroductionToAlgorithms;
+
+public class PseudocodeBubbleSortAlgorithm {
+    /*
+
+        for i = 0 to A.length-2
+            for j = 0 to A.length-2-1
+                if A[j] > A[j +1]
+                    tmp = A [j+1]
+                    A [j+1]  = A [j]
+                    A [j] = tmp
+
+        This will translate to this
+        for (int i = 0; i < A.length-2; i++) {
+            for (int j =0; j <A.length-2-i; j++) {
+                if (A[j] > A[j+1] {
+                    tmp = A [j+1]
+                    A [j+1]  = A [j]
+                    A [j] = tmp
+     */
+}
---
+++ src/IntroductionToDataStructuresAndAlgorithmsInJava/IntroductionToAlgorithms/TheBigONotation.java
@@ -0,0 +1,9 @@
+package IntroductionToDataStructuresAndAlgorithmsInJava.IntroductionToAlgorithms;
+
+public class TheBigONotation {
+    /*
+        The Big O Notation
+            used to classify algorithms according to how their runtime requirements grow as the input size grows
+            it shows the upward boundary of an algorithm
+     */
+}
