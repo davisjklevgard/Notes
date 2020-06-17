fd74bf1f8970b601890eaf8c25dfbd8cbb328467
Davis Klevgard <davisjklevgard@gmail.com>
2020-06-17 09:45:03 -0500

    Adding what I have done for today

---
+++ src/IntroductionToDataStructuresAndAlgorithmsInJava/BasicSortingAndSearchAlgorithms/SearchingElementsInAnUnorderedArray.java
@@ -0,0 +1,7 @@
+package IntroductionToDataStructuresAndAlgorithmsInJava.BasicSortingAndSearchAlgorithms;
+
+public class SearchingElementsInAnUnorderedArray {
+    /*
+        Accessing an array element through the index is very fast. It is not dependent on size
+     */
+}
--- src/IntroductionToDataStructuresAndAlgorithmsInJava/BasicSortingAndSearchAlgorithms/StableVSUnstableSorts.java
+++ src/IntroductionToDataStructuresAndAlgorithmsInJava/BasicSortingAndSearchAlgorithms/StableVSUnstableSorts.java
@@ -2,6 +2,20 @@ package IntroductionToDataStructuresAndAlgorithmsInJava.BasicSortingAndSearchAlg
 
 public class StableVSUnstableSorts {
     /*
+        Stable sort
+            the order of the elements in which they occurred in a list or edits is maintained even after the sort
+        Unstable sort
+            the order is not maintained,
+        THis occurs if there are say two elements that are the same in an array
+        take this list: 5 3 2 1  5. 8 10
+
+        Stable
+            1 2 3 5 5. 8 10
+        Unstable
+            1 2 3 5. 5 8 10
+
+        If one is to write the insertion sort algorithm in a way so that the part that moves the number is not equal to
+        but just greater than, the list will be a stable list
 
      */
 }
