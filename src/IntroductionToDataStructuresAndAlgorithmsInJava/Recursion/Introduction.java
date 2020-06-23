06c06d47acc72aa295a901868311de8989ef7b28
Davis Klevgard <davisjklevgard@gmail.com>
2020-06-23 08:39:33 -0500

    Adding what I have done for today

---
+++ src/IntroductionToDataStructuresAndAlgorithmsInJava/Recursion/Introduction.java
@@ -0,0 +1,42 @@
+package IntroductionToDataStructuresAndAlgorithmsInJava.Recursion;
+
+public class Introduction {
+    /*
+        Euclid's Algorithm is a recursive algorithm as it calls itself
+
+        Recursion
+            when a method calls itself from within the same method
+
+        Breaking Condition:
+            Very important to have one in recursive calls
+            a statement that will close the method and has the program moves past it.
+
+        Factorial of a Method
+            factorial(n) = n*(n-1)*(n-2)*.....1
+            factorial(n) = n *factorial(n-1)
+
+            Iterative Way
+                public in factorial (int N) {
+                    int result = 1;
+                    for (int i = 1; i <= n; i++) {
+                        result *= i;
+                    }
+                    return result;
+                }
+
+            Recursive Way
+                public static int factorial (int n) {
+                    if(n == 0) return 1;
+                    return n*factorial(n-1);
+                }
+     */
+
+    public static int factorial (int n) {
+        if (n == 0) return 1;
+        return n*factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
+}
